f7edc8142bd9b30ebfcef2e3cdd3d125b93c3449
Marques <gaston.marques@gmail.com>
2025-01-28 09:46:26 -0500

    basic search func

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ def add_person():
     person = {"name": name,"age": age,"email": email}
     return person
 
-## Delete Function
-def delete_contact(people):
+def display_people(people):
     for i, person in enumerate(people): # enumerate give syou the index as well as the element for every single element inside of your list
         print(i + 1,":", person["name"], "|", person["age"], "|", person["email"])
 
+## Delete Function
+def delete_contact(people):
+    display_people(people)
+
     while True:
-        print("Contact list size:", len(people))
         number = input("Enter a number to delete: ")
         try:
             number = int(number)
@@ -26,16 +28,46 @@ def delete_contact(people):
         except:
             print("Invalid number")
 
-
     people.pop(number - 1)
     print("Person deleted")
 
+## Basic Search Function
+def search_contact(people):
+    search_name = input("Search for a name: ").lower()
+    results = []
+
+    for person in people:
+        name = person["name"]
+        if search_name in name.lower():
+            results.append(person)
+        
+    display_people(results)
+        
+
+
+
+#App Start
 print("Hi, welcome to the Contact Management System")
 print()
 
-people = []
+people = [
+    {"name": "Tim",
+     "age" : 24,
+     "email": "tim@nomail.com"
+     },
+     {"name": "Timothy",
+     "age" : 27,
+     "email": "timothy@nomail.com"
+     },
+     {"name": "Joe",
+     "age" : 28,
+     "email": "joe@nomail.com"
+     }
+]
 
 while True:
+    print()
+    print("Contact list size:", len(people))
     command = input("You can 'Add', 'Delete', or 'Search' and 'Q' for quit: ").lower()
 
     if command == "add":
@@ -45,7 +77,7 @@ while True:
     elif command == "delete":
         delete_contact(people)
     elif command == "search":
-        pass
+        search_contact(people)
     elif command == "q":
         break
     else:
